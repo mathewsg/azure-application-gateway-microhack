bba196e3beccb677c45f32b37c4780484de26a49
Mathews George <mathewsg6@gmail.com>
2024-09-09 16:41:10 +0100

    Update azurefirewall.tf to Standard

--- templates/azurefirewall.tf
+++ templates/azurefirewall.tf
@@ -37,7 +37,7 @@ resource "azurerm_firewall" "hub-azure-firewall" {
   location            = azurerm_resource_group.hub-rg.location
   resource_group_name = azurerm_resource_group.hub-rg.name
   sku_name            = "AZFW_VNet"
-  sku_tier            = "Premium"
+  sku_tier            = "Standard"
   firewall_policy_id  = azurerm_firewall_policy.hub-azfirewall-policy.id
 
   ip_configuration {
